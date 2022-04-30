f89113b24e5280516c3cf990ede26cf57ae5334d
yu04038 <yu04038@gmail.com>
2022-05-01 01:50:31 +0900

    2750.kt

---
+++ DataStructure/src/main/kotlin/zip.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    var string = "aabbaccc"
+    solution(string)
+}
+
+fun solution(s: String): Int {
+    var answer = 0
+
+    for (i in s.indices) {
+
+    }
+    return answer
+}
