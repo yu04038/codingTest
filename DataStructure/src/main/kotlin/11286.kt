0c3c7670c67fc5b3db5ff6654c58505508c57901
yu04038 <yu04038@gmail.com>
2022-06-02 00:42:09 +0900

    class 3++

---
+++ DataStructure/src/main/kotlin/11286.kt
@@ -0,0 +1,26 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+fun main() {
+
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val num = br.readLine().toInt()
+    val arr = PriorityQueue<Int>( )
+
+    for (i in 0 until num) {
+        val a = br.readLine().toInt()
+
+        when (a) {
+            0 -> {
+                if (arr.isEmpty()) {
+                    println("0")
+                } else {
+                    println(arr.peek())
+                    arr.remove()
+                }
+            }
+            else -> arr.add(a)
+        }
+    }
+}
